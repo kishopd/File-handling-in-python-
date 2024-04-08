50d1b3112cc03642a4e9af8808b60ac1ffc177ff
kishopd <kishore.1804040@srit.org>
2024-04-08 15:36:00 +0530

    changes

---
+++ basics_panda.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_excel("D:/demoexcel.xlsx")
+print(df)
---
+++ open_excelfile.py
@@ -0,0 +1,4 @@
+""" This code is opening a excel code in python """
+import pandas as pd
+df=pd.read_excel('')    
+print(df)
