99a870332bfebc0966631feb31940ee18090db34
kishopd <kishore.1804040@srit.org>
2024-04-10 13:46:49 +0530

    pandas chnged

--- read_pdffile.py
+++ read_pdffile.py
@@ -1,1 +1,21 @@
-print("Hello world")
+""" extracting the table c
+extracting the number of pages
+extracting the particular pages
+extracting the text only 
+extract datas from the password protected files 
+"""
+import pdfplumber
+# read the files here
+result = pdfplumber.open('ankush_resume.pdf')
+# for i in result.pages:
+#     print(i.extract_text())
+
+
+# converting them into an pdf file to text file 
+text=''
+for i in result.pages:
+    page_text= i.extract_text()
+    text= text + page_text  
+
+with open('output.txt', 'w', encoding='utf-8') as f:
+    f.write(text)
