c756b361a7d8685e7a68e7bc5f6cb8b4cee91028
kishopd <kishore.1804040@srit.org>
2024-04-12 11:10:26 +0530

    final_commit

--- open_excelfile.py
+++ open_excelfile.py
@@ -1,4 +1,15 @@
 """ This code is opening a excel code in python """
-import pandas as pd
-df=pd.read_excel('')    
-print(df)
+# import pandas as pd
+# df = pd.read_excel('D:\Basic Python Learnings\pandas\demoexcel.xlsx')
+# df.to_csv()
+a=100
+b=20
+while True:
+    if a and b!=0:
+        print(a//b)
+        break
+    else:
+        print("zero division eroor")
+        
+
+    
